adf36a76470c7108b5b1f508c01c2e5a48ca7aad
nikolajsvv <nikolajsvv@gmail.com>
2023-06-02 10:31:19 -0400

    Menu & Menu Toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
+
 import './styles/App.css'
-// import StanfordBanner from './components/Banner/StanfordBanner'
-// import Header from './components/Header/Header'
-// import NavBar from './components/NavBar/NavBar'
 import NavBar from './components/NavBar/NavBar'
-import HowDidWeGetHere from './components/Sections/HowDidWeGetHere/HowDidWeGetHere'
-import WhereAreWe from './components/Sections/WhereAreWe/WhereAreWe'
-import WhereDoWeGoFromHere from './components/Sections/WhereDoWeGoFromHere/WhereDoWeGoFromHere'
-import HowDoWeMakeSenseOfItAll from './components/Sections/HowDoWeMakeSenseOfItAll/HowDoWeMakeSenseOfItAll'
+// import HowDidWeGetHere from './components/Sections/HowDidWeGetHere/HowDidWeGetHere'
+// import WhereAreWe from './components/Sections/WhereAreWe/WhereAreWe'
+// import WhereDoWeGoFromHere from './components/Sections/WhereDoWeGoFromHere/WhereDoWeGoFromHere'
+// import HowDoWeMakeSenseOfItAll from './components/Sections/HowDoWeMakeSenseOfItAll/HowDoWeMakeSenseOfItAll'
 
 const App = () => {
   return (
     <>
     <NavBar/>
-    <WhereAreWe/>
-    <HowDidWeGetHere />   
-    <HowDoWeMakeSenseOfItAll/>
-    <WhereDoWeGoFromHere/>
+    {/* <WhereAreWe />
+    <HowDidWeGetHere />
+    <WhereDoWeGoFromHere />
+    <HowDoWeMakeSenseOfItAll /> */}
     </>
   )
 }
