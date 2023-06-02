1374373b2cb9c7d4b9f9f0e9342c80ef8de05c06
nikolajsvv <nikolajsvv@gmail.com>
2023-06-02 15:26:51 -0400

    EssayPreview Styling

--- src/components/Essay/EssayPreview.jsx
+++ src/components/Essay/EssayPreview.jsx
@@ -1,22 +1,29 @@
 import EssayFullView from './EssayFullView';
 import PropTypes from 'prop-types';
+import image1 from '../../assets/philip-jahn-r_rnWGV64Ac-unsplash.jpg'
 import { useState } from 'react'
 
 
 const EssayPreview = ({essay}) => {
   const [showFullView, setShowFullView] = useState(false);
 
-  const handleViewClick = () => {
-    setShowFullView(true);
-  }
+  // const handleViewClick = () => {
+  //   setShowFullView(true);
+  // }
 
   return (
     <>
-    <div className='bg-white flex flex-col rounded shadow-md mx-auto w-full font-source-serif-pro sm:w-1/2' onClick={handleViewClick}>
-      <h2 className='px-4 pt-2 text-center text-2xl uppercase'>{essay.title}</h2>
-      <p className='px-4 text-center font-light '>{essay.author}</p>
-      
-      <p className='px-4 py-2 font-light '>{essay.content.substring(0,100)}...</p>
+    
+    <div className='group bg-primary-bg p-5 flex flex-col mx-auto w-full sm:w-1/3'>
+      <img src={image1} className='bg-gray-100 rounded-md aspect-square transition-all ease-in-out delay-100 object-cover h-72 hover:scale-105'/>
+        <h2 className='pt-2 text-left text-2xl uppercase font-souce-sans-pro text-primary-green'>
+          <span className='bg-gradient-to-r from-green-300 to-green-200 bg-[length:0px_10px] bg-left-bottom bg-no-repeat duration-500 hover:bg-[length:100%_10px] group-hover:bg-[length:100%_10px]'>
+            {essay.title}
+          </span>
+        </h2>
+        <p className='text-left font-extralight font-souce-sans-pro'>{essay.author}</p>
+        <p className='py-2 font-light font-source-serif-pro'>{essay.content.substring(0,200)}...</p>
+
     </div>
 
     {showFullView && (
@@ -33,7 +40,8 @@ EssayPreview.propTypes = {
   essay: PropTypes.shape({
     title: PropTypes.string,
     author: PropTypes.string,
-    content: PropTypes.string
+    content: PropTypes.string,
+    type: PropTypes.string,
   }).isRequired,
 }
 export default EssayPreview
