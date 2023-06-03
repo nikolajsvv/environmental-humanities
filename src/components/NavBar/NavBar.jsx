9896287171b71c0fbc3bfb3776691f1c42d182e6
nikolajsvv <nikolajsvv@gmail.com>
2023-06-02 22:23:56 -0400

    Styling changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import NavItem from './NavItem'
+// import NavItem from './NavItem'
 import useMenuAnimation from './useMenuAnimation'
 import MenuToggle from './MenuToggle'
 import Menu from './Menu'
@@ -20,18 +20,7 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="bg-primary-bg w-screen fixed top-0 z-50 hidden lg:block">
-        <motion.div style={{ scaleX: scrollYProgress }} className='absolute top-0 left-0 right-0 h-10 z-10 bg-primary-green transform origin-left'/>
-        <div className="relative h-10 md:flex justify-around items-center text-sm lg:text-md xl:text-lg font-source-sans-pro font-light uppercase border-2 border-black border-opacity-75 z-20">
-          {sections.map(section => (
-              <NavItem
-              key={section.id}
-              section={section}
-            />
-            ))}
-        </div>
-      </nav>
-
+      {/* <motion.div style={{ scaleX: scrollYProgress }} className='absolute top-0 left-0 right-0 h-10 z-10 bg-primary-green transform origin-left'/> */}
       <div ref={scope} className='lg:hidden'>
         <Menu setIsOpen={setIsOpen} sections={sections}/>
         <MenuToggle toggle={() => setIsOpen(!isOpen)}/>
