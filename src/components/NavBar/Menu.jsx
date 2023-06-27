10352ccca88de862be89ec883257c544d1faf6d2
nikolajsvv <nikolajsvv@gmail.com>
2023-06-26 22:06:35 -0400

    Menu onload animation

--- src/components/NavBar/Menu.jsx
+++ src/components/NavBar/Menu.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { Link } from "react-scroll";
-
+import { motion } from 'framer-motion';
 const Menu = ({setIsOpen, sections}) => {
   return (
-    <nav className="fixed top-0 left-0 bottom-0 w-[400px] z-50 bg-dark-green pt-[100px] will-change-transform duration-300">
+    <motion.nav className="fixed top-0 left-0 bottom-0 w-[400px] z-50 bg-dark-green pt-[100px] will-change-transform duration-300" initial={{ x: "-100%" }}>
       <ul className="flex flex-col gap-10 p-[15px] text-light-beige font-bold text-[30px] uppercase font-source-sans-pro cursor-pointer">
         {sections.map((section) => {
           if (section.title !== '') {
@@ -24,7 +24,7 @@ const Menu = ({setIsOpen, sections}) => {
           }
         })}
       </ul>
-    </nav>
+    </motion.nav>
   )
 }
 Menu.propTypes = {
