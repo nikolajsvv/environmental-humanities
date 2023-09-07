b360fb82fefd8e8ba699dd8a623f3d5d47bdcb2f
nikolajsvv <nikolajsvv@gmail.com>
2023-09-06 22:16:41 -0400

    Remove about sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 
 import './styles/App.css'
-import content1 from './assets/Submissions/About/section_1.json'
-import content2 from './assets/Submissions/About/section_2.json'
-import content3 from './assets/Submissions/About/section_3.json'
-import content4 from './assets/Submissions/About/section_4.json'
 import NavBar from './components/NavBar/NavBar'
-import About from './components/Sections/About/About'
 import Launch from './components/Sections/Launch/Launch'
 import HowDidWeGetHere from './components/Sections/HowDidWeGetHere/HowDidWeGetHere'
 import WhereAreWe from './components/Sections/WhereAreWe/WhereAreWe'
@@ -19,10 +14,6 @@ const App = () => {
     <>
     <NavBar/>
     <Launch/>
-    <About content={content1} id="about-1"/>
-    <About content={content2} id="about-2"/>
-    <About content={content3} id="about-3"/>
-    <About content={content4} id="about-4"/>
     <WhereAreWe />
     <HowDidWeGetHere />
     <WhereDoWeGoFromHere />
